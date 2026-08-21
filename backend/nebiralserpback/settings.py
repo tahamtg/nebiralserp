be23dcbc14652aa7189f3228e9dd976e45b01c58
taha <tahaghataei@gmail.com>
2026-08-21 15:46:38 +0330

    configure persistent storage

--- backend/nebiralserpback/settings.py
+++ backend/nebiralserpback/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'nebiralserpback.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
     }
 }
 
